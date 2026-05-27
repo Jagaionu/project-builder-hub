443e8faeb794f3ecebf41083d6370b37e0d4c88c
Jagaionu <Mugur_ionut92@yahoo.com>
2026-05-27 11:04:12 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -9,6 +10,7 @@ export default defineConfig({
     tanstackStart({
       server: { entry: "src/server.ts" },
     }),
+    tailwindcss(),
     react(),
     tsconfigPaths(),
     VitePWA({
